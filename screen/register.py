c1ea2ada5b1d405f84b6e3623a7a3cbf6aa0e144
shellyneu <shellyneuriska@gmail.com>
2025-07-16 15:53:42 +0700

    terakhir ya Allah

--- screen/register.py
+++ screen/register.py
@@ -6,7 +6,7 @@ class RegisterPage(tk.Frame):
         super().__init__(parent, bg="white")
         self.controller = controller
 
-        canvas = tk.Canvas(self, width=1024, height=550, bg="white", highlightthickness=0)
+        canvas = tk.Canvas(self, width=1024, height=500, bg="white", highlightthickness=0)
         canvas.pack(fill="both", expand=True)
 
         bg_image = Image.open("./assets/background.png").convert("RGBA")
@@ -17,10 +17,10 @@ class RegisterPage(tk.Frame):
         canvas.logo = ImageTk.PhotoImage(logo)
         canvas.create_image(23, 22, image=canvas.logo, anchor="nw")
 
-        canvas.create_text(500, 170, text="Register", fill="#2f4f12", font=("Inter", 28, "bold"))
+        canvas.create_text(500, 150, text="Register", fill="#2f4f12", font=("Inter", 28, "bold"))
         
         nama_label = tk.Label(self, text="Nama", font=("Inter", 14, "bold"), bg="white", fg="#2f4f12")
-        nama_label.place(x=305, y=230)
+        nama_label.place(x=305, y=210)
         
         vcmd_letters = (self.register(self.validate_letters), "%P")
         self.nama_entry = tk.Entry(
@@ -37,10 +37,10 @@ class RegisterPage(tk.Frame):
             validate="key",
             validatecommand=vcmd_letters
         )
-        self.nama_entry.place(x=295, y=260, width=450, height=40)
+        self.nama_entry.place(x=295, y=240, width=450, height=40)
 
         nim_label = tk.Label(self, text="NIM", font=("Inter", 14, "bold"), bg="white", fg="#2f4f12")
-        nim_label.place(x=305, y=315)
+        nim_label.place(x=305, y=295)
 
         vcmd_numbers = (self.register(self.validate_numbers), "%P")
         self.nim_entry = tk.Entry(
@@ -57,7 +57,7 @@ class RegisterPage(tk.Frame):
             validate="key",
             validatecommand=vcmd_numbers
         )
-        self.nim_entry.place(x=295, y=345, width=450, height=40)
+        self.nim_entry.place(x=295, y=325, width=450, height=40)
 
         
         button_img = Image.open("./assets/button.png").resize((200, 50)).convert("RGBA")
@@ -66,12 +66,12 @@ class RegisterPage(tk.Frame):
         cancel_btn = tk.Button(self, image=canvas.button, text="Cancel", font=("Inter", 12, "bold"), fg="#2f4f12",
                                 compound="center", bd=0, bg="white", activebackground="white",
                                 command=lambda: controller.show_frame("StartPage"))
-        cancel_btn.place(x=80, y=480, width=200, height=50)
+        cancel_btn.place(x=80, y=430, width=200, height=50)
 
         register_btn = tk.Button(self, image=canvas.button, text="Register", font=("Inter", 12, "bold"), fg="#2f4f12",
                                     compound="center", bd=0, bg="white", activebackground="white",
                                     command=lambda: controller.show_frame("LoginPage"))
-        register_btn.place(x=750, y=480, width=200, height=50)
+        register_btn.place(x=750, y=430, width=200, height=50)
 
     def reset_form(self):
         self.nama_entry.delete(0, tk.END)
